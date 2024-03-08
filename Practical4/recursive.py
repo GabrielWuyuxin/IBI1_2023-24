0cf328fa997f036817aa057a5653af8ff32f7556
Gabriel Wu <13059712159@qq.com>
2024-03-08 10:41:58 +0800

    update code for recursive.py

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,7 +1,7 @@
-#a,b=4,11 represents the first two values
-#i represents the ordinal number, i+=1,a=2*b+3
+#let begin value a be 0.5, represents the "a0", so that the first "a" after caculation, which is 4, becomes "a1".
+#i represents the ordinal number, when i adds 1, a is caculated as multiple by 2 and add 3.
 a=0.5
 i=1
-for i in (1,7,1):
+for i in range(1,6,1):#the 6 can be changed if we want more munber to be printed
     a=2*a+3
     print(a)
