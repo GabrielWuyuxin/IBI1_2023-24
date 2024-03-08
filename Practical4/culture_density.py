042a6f1504a8af2a3a74c52dfe4e8fea4fc590c8
GabrielWuyuxin <161407581+GabrielWuyuxin@users.noreply.github.com>
2024-03-08 09:57:48 +0800

    Add pseudocode to culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,5 +1,6 @@
-#a=density at the beginning, s on day1 =0
-#every time a=a*2,s=s+1
+#let a be the density at the beginning, let s be 0 referring to day1, so on day2 s will be 1.
+#every time when one day pasts, the density doubles.
+#when a reach 0.9, s refers to the number of the day that you can go out for break.
 a=0.05
 s=0
 while a<=0.9:
