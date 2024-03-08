33ef092b73c54cfa457277ff4a2e486d580bfcbf
GabrielWuyuxin <161407581+GabrielWuyuxin@users.noreply.github.com>
2024-03-08 10:12:39 +0800

    Update pseudocode for recursive.py

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,5 +1,5 @@
-#a,b=4,11 represents the first two values
-#i represents the ordinal number, i+=1,a=2*b+3
+#let begin value a be 0.5, represents the "a0", so that the first "a" after caculation, which is 4, becomes "a1".
+#i represents the ordinal number, when i adds 1, a is caculated as multiple by 2 and add 3.
 a=0.5
 i=1
 for i in (1,7,1):
